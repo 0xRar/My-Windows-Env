3585c92a334f19a629419230f1d209d13a169035
0xRar <33517160+0xRar@users.noreply.github.com>
2025-03-08 16:53:52 +0300

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,7 +10,6 @@ config.color_scheme = "Abernathy"
 -- for more https://wezterm.org/colorschemes/a/index.html
 
 -- Changing the default shell to pwsh instead of cmd
--- Get-Command pwsh
 config.default_prog = { "C:\\Program Files\\WindowsApps\\Microsoft.PowerShell_7.5.0.0_x64__8wekyb3d8bbwe\\pwsh.exe" }
 
 -- Rar Key Binds
@@ -29,11 +28,18 @@ config.keys = {
 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
 
-	-- Use F11 for maximizing a pane
+	-- Use F11 for maximizing a pane - or use CTRL + SHIFT + Z
 	{
 		key = "F11",
 		mods = "",
 		action = wezterm.action.TogglePaneZoomState,
+	},
+
+	-- Use CTRL + SHIFT + Q to close current pane
+	{
+		key = "Q",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
 	},
 }
 
